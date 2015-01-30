e7a8f5e4087de240b10c86970964f11e9e56ec0f
Tom Duffield <tom@chef.io>
2015-01-30 09:09:47 -0600

    Play dramatic HBO opening

---
+++ spec/unit/recipes/default_spec.rb
@@ -0,0 +1,9 @@
+require 'chefspec'
+
+describe 'game_of_thrones::default' do
+  let(:george_rr_martin) { ChefSpec::SoloRunner.converge(described_recipe) }
+
+  it 'will be a blood bath' do
+    expect(george_rr_martin).to run_execute('all_your_favorite_characters')
+  end
+end
