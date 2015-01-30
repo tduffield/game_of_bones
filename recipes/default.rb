ea5209e89aca5a75549397fcf97576a5c0d28a34
Tom Duffield <tom@chef.io>
2015-01-30 11:05:19 -0600

    Good, that twerp deserved it.

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,7 +7,7 @@
 #execute 'ned_stark'
 #execute 'robb_stark'
 #execute 'oberyn_martell'
-#execute 'joffrey_baratheon'
+execute 'joffrey_baratheon'
 #execute 'rently_baratheon'
 #execute 'robert_baratheon'
 #execute 'catelyn_stark'
