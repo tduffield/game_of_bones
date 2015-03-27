c5396f8667df5816799385150eee540573605369
Tom Duffield <tom@chef.io>
2015-03-27 14:12:41 -0500

    merp

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -4,6 +4,6 @@ describe 'game_of_thrones::default' do
   let(:george_rr_martin) { ChefSpec::SoloRunner.converge(described_recipe) }
 
   it 'will be a blood bath' do
-    expect(george_rr_martin).to run_execute('joffrey_baratheon')
+    expect(george_rr_martin).to run_execute('ned_stark')
   end
 end
